0b0395e9bea1b6b55b6807082bfc436b4e7441a3
Alex Gschnitzer <alex.gschnitzer@protonmail.com>
2021-05-27 20:57:43 +0200

    Initial commit
    Added dependencies and adjusted gitignore file

---
+++ src/main/java/com/example/multitenant/MultitenantApplication.java
@@ -0,0 +1,13 @@
+package com.example.multitenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultitenantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/multitenant/MultitenantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multitenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
